cfbadbde750e089ccc4c009ace7f7c01a5a5c9a8
dammw305 <batoidam6@gmail.com>
2025-09-28 09:55:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,16 +3,16 @@
 module.exports = {
   TOKEN: "",
   language: "en",
-  ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
-  spotifyClientId : "",
-  spotifyClientSecret : "",
+  ownerID: ["1366026724743053382", ""], 
+  mongodbUri : "mongodb+srv://codex-us2:codex-us2@codex-us2.62zm1.mongodb.net/?retryWrites=true&w=majority&appName=codex-us2",
+  spotifyClientId : "85aab1d51a174aad9eed6d7989f530e6",
+  spotifyClientSecret : "b2ad05aa725e434c88776a1be8eab6c2",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
   activityName: "YouTube Music", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  SupportServer: "https://discord.gg/akzfb4c7w5",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
